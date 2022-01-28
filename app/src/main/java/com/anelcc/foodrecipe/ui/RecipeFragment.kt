d0351a604c2b737b6db4644213cc4f2f2ccb7da2
AnelCC <anel.calvo.cervantes@gmail.com>
2022-01-27 19:02:19 -0500

    Bind the fragment

--- app/src/main/java/com/anelcc/foodrecipe/ui/RecipeFragment.kt
+++ app/src/main/java/com/anelcc/foodrecipe/ui/RecipeFragment.kt
@@ -1,4 +1,4 @@
-package com.anelcc.foodrecipe
+package com.anelcc.foodrecipe.ui
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
@@ -6,9 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.anelcc.foodrecipe.ui.RecipeViewModel
+import com.anelcc.foodrecipe.databinding.RecipeFragmentBinding
 
 class RecipeFragment : Fragment() {
+
+    private var _binding: RecipeFragmentBinding? = null
+    // This property is only valid between onCreateView and
+// onDestroyView.
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = RecipeFragment()
@@ -16,9 +21,13 @@ class RecipeFragment : Fragment() {
 
     private lateinit var viewModel: RecipeViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.recipe_fragment, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = RecipeFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -27,4 +36,8 @@ class RecipeFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
